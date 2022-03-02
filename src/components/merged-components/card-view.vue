daabbcd295dc01355a787302c1891ad3724df259
jeelpatel231 <jeelpatel231@gmail.com>
2022-03-02 13:56:52 +0530

    disable nested scroll

--- src/components/merged-components/card-view.vue
+++ src/components/merged-components/card-view.vue
@@ -93,8 +93,6 @@ export default {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-evenly;
-  max-height: calc(100vh - 60px);
-  overflow: scroll;
 
   &::-webkit-scrollbar {
     display: none;
