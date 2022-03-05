1e019f47b3be91ff4a3dbbdffeb36224281b6676
jeelpatel231 <jeelpatel231@gmail.com>
2022-03-05 16:17:11 +0530

    temp watch component added

--- src/components/small-components/watchtab.vue
+++ src/components/small-components/watchtab.vue
@@ -1,10 +1,12 @@
 <template>
+    <div>{{id}}</div>
     WATCH COMPONENT
 </template>
 
 <script>
 export default{
     name:"watchtab",
+    props:["id","details"] // details here to suppress prop pass warning
 }
 </script>
 
