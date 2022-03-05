4e37c1d8d040935b07043d81b0750337b8075928
jeelpatel231 <jeelpatel231@gmail.com>
2022-03-05 16:11:09 +0530

    re-render only of id changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <router-view v-slot="{ Component }">
+  <router-view :key="this.$route.params.id" v-slot="{ Component }">
+            <!-- ^ only re-render on root paths and not in nested -->
     <keep-alive max="5">
       <component :is="Component" />
     </keep-alive>
