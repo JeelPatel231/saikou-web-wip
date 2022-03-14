d206fee9122c83bb67076262d7b6d2bad28a04e8
jeelpatel231 <jeelpatel231@gmail.com>
2022-03-14 21:52:46 +0530

    remove duplicate

--- src/components/merged-components/card-view.vue
+++ src/components/merged-components/card-view.vue
@@ -164,7 +164,6 @@ export default {
   margin: 5.5rem 10px 10px 10px;
   cursor: pointer;
   box-shadow: 2px 2px 2px 0 rgba(0, 0, 0, 0.1);
-  box-shadow: 2px 2px 2px 0 rgba(0, 0, 0, 0.1);
   &.end {
     right: 0;
   }
